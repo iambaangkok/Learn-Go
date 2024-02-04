f4e97f891fa62e3ddfaf586bcc7d4ddce719fc97
Baangkok Vanijyananda <iambaangkok@gmail.com>
2024-02-04 16:39:25 +0700

    add:quiz game: timer (part 2)

--- src/github.com/iambaangkok/gophercises 01 quiz game/Main.go
+++ src/github.com/iambaangkok/gophercises 01 quiz game/Main.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"strings"
 	"time"
+	// "sync"
 )
 
 const workDir string = "./src/github.com/iambaangkok/gophercises 01 quiz game/"
@@ -22,12 +23,34 @@ func shuffle(data [][]string) {
 	}
 }
 
+// var wg sync.WaitGroup
+
+var currentQuestion int = 0
+
+func countDown(ch chan struct{}, seconds int, questionId int) {
+	time.Sleep(time.Duration(seconds) * time.Second)
+	if (currentQuestion == questionId) {
+		ch <- struct{}{}
+	}
+}
+
+func readString(ch chan string, inputReader *bufio.Reader) {
+	text, _ := inputReader.ReadString('\n')
+	ch <- text
+}
+
 func main() {
 
-	randomizeFlag := flag.Bool("rand", false, "randomize quiz order")
+	// Channels
+	readStringCh := make(chan string)
+	timeLimitCh := make(chan struct{}) 
+
+	// Declare flags
+	timeLimitPtr := flag.Int("time-limit", 30, "time limit per question (seconds)")
+	randomizePtr := flag.Bool("rand", false, "randomize quiz order")
 	flag.Parse()
 
-
+	// Read file
 	const problemsFileName = "problems.csv"
     file, err := os.Open(workDir + problemsFileName)
     if err != nil {
@@ -42,17 +65,24 @@ func main() {
 		fmt.Println("Error while reading records")
 	}
 
+	if *randomizePtr {
+		shuffle(problems)
+	}
+
+	// Press Enter to start
+	inputReader := bufio.NewReader(os.Stdin)
+	fmt.Printf("QUIZ GAME: Press Enter to start! You have %v seconds for each question.", *timeLimitPtr)
+	inputReader.ReadString('\n')
+		
+
 	score := 0
 	problemCount := len(problems)
 	invalidFormatProblemCount := 0 
 
-	inputReader := bufio.NewReader(os.Stdin)
-
-	if *randomizeFlag {
-		shuffle(problems)
-	}
-
 	for i, problem := range problems {
+		// wg.Add(1)
+		currentQuestion = i
+
 		if len(problem) != 2 {
 			fmt.Printf("Problem %v is in invalid format", i)
 			invalidFormatProblemCount++
@@ -63,12 +93,25 @@ func main() {
 		solution := problem[1]
 
 		fmt.Printf("#%v %s: ", i, question)
-		text, _ := inputReader.ReadString('\n')
+		go countDown(timeLimitCh, *timeLimitPtr, i)
+		go readString(readStringCh, inputReader)
+
+		text := ""
+		timedOut := false
+		select {
+			case t := <- readStringCh:
+				text = t
+			case _ = <- timeLimitCh:
+				timedOut = true
+		}
+
 		textTrimmed := strings.TrimSpace(text)
 		solutionTrimmed := strings.Replace(solution, " ", "", -1)
 
 		fmt.Printf("  sol: %v - (%v) | ans: %v - (%v)\n", solution, len(solution), textTrimmed, len(textTrimmed))
-		if strings.Compare(strings.ToLower(solutionTrimmed), strings.ToLower(textTrimmed)) == 0 {
+		if timedOut {
+			fmt.Println(">>> timed out.")
+		} else if strings.Compare(strings.ToLower(solutionTrimmed), strings.ToLower(textTrimmed)) == 0 {
 			fmt.Println(">>> correct!")
 			score++
 		} else {
